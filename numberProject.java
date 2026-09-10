6ee26c13af64cba5c00b64c9a2fa3e3c904a8b80
KingPikqchu <pikalam1016@gmail.com>
2026-09-10 11:23:25 -0700

    updated with print statements

--- numberProject.java
+++ numberProject.java
@@ -1,71 +1,122 @@
-public class numberProject {
+public class numberproject {
     public static void main(String[] args)
     {
-    // Positive Integer - Dylan 
+    // Positive Integer  
         int val = 2;
         int updval;
 
+
+        System.out.println("Student 1: Aarav Aggarwal");
         System.out.println("Original value: " + val);
+        System.out.println("First: " + val + " * 2");
         updval = val * 2;
+        System.out.println("Second: " + updval + " + 6");
         updval = updval + 6;
+        System.out.println("Third: " + updval + " / 2");
         updval = updval / 2;
+        System.out.println("Fourth: " + updval + " - " + val);
         updval = updval - val;
 
         System.out.println("AUTOGRADER CALCULATING . . . " + updval + "/5");
+        System.out.println("");
+        System.out.println("");
+
     
-    // Negative Integer - Dylan 
+    // Negative Integer  
         int val2 = -2;
         int updval2; 
+        System.out.println("Student 2: Arnesh Mahata");
         System.out.println("Original value: " + val2);
+        System.out.println("First: " + val2 + " * 2");
         updval2 = val2 * 2;
+        System.out.println("Second: " + updval2 + " + 6");
         updval2 = updval2 + 6;
+        System.out.println("Third: " + updval2 + " / 2");
         updval2 = updval2 / 2;
+        System.out.println("Fourth: " + updval2 + " - " + val2);
         updval2 = updval2 - val2;
 
         System.out.println("AUTOGRADER CALCULATING . . . " + updval2 + "/5");
+        System.out.println("");
+        System.out.println("");
 
-    // 0 - Tiger 
+    // Zero Value
         int val3 = 0;
         int updval3; 
+        System.out.println("Student #3: Vandy");
         System.out.println("Original value: " + val3);
+        System.out.println("First: " + val3 + " * 2");
         updval3 = val3 * 2;
+        System.out.println("Second: " + updval3 + " - 6");
         updval3 = updval3 + 6;
+        System.out.println("Third: " + updval3 + " / 2");
         updval3 = updval3 / 2;
+        System.out.println("Foruth: " + updval3 + " - " + val3);
         updval3 = updval3 - val3;
 
         System.out.println("AUTOGRADER CALCULATING . . . " + updval3 + "/5");
+        System.out.println("");
+        System.out.println("");
+
     
-    // 1 - Tiger
+
+    // One Value
         int val4 = 1;
         int updval4; 
+        System.out.println("Student #4: Vansh");
         System.out.println("Original value: " + val4);
+        System.out.println("First: " + val4 + " * 2");
         updval4 = val4 * 2;
+        System.out.println("Second: " + updval4 + " + 6");
         updval4 = updval4 + 6;
+        System.out.println("Third: " + updval4 + " / 2");
         updval4 = updval4 / 2;
+        System.out.println("Fourth: " + updval4 + " - " + val4);
         updval4 = updval4 - val4;
 
         System.out.println("AUTOGRADER CALCULATING . . . " + updval4 + "/5");
+        System.out.println("");
+        System.out.println("");
 
-    // Positive Double - 1.5 - Arjun
-        double val5 = 1.5;
+
+    // Positive Double 4.5 
+        double val5 = 4.5;
         double updval5; 
+        System.out.println("Student #5: Tyler");
         System.out.println("Original value: " + val5);
+        System.out.println("First: " + val5 + " * 2");
         updval5 = val5 * 2;
+        System.out.println("Second: " + updval5 + " + 6");
         updval5 = updval5 + 6;
+        System.out.println("Third: " + updval5 + " / 2");
         updval5 = updval5 / 2;
+        System.out.println("Fourth: " + updval5 + " - " + val5);
         updval5 = updval5 - val5;
 
         System.out.println("AUTOGRADER CALCULATING . . . " + (int)updval5 + "/5");
+        System.out.println("");
+        System.out.println("");
+        
 
-    // Negative Double -1.5 - Arjun
-        double val6 = -1.5;
+
+    // Negative Double -4.5 
+        double val6 = -4.5;
         double updval6; 
+        System.out.println("Student #6: Anya");
         System.out.println("Original value: " + val6);
+        System.out.println("First: " + val6 + " * 2");
         updval6 = val6 * 2;
+        System.out.println("Second: " + updval6 + " + 6");
         updval6 = updval6 + 6;
+        System.out.println("Third: " + updval6 + " / 2");
         updval6 = updval6 / 2;
+        System.out.println("Fourth: " + updval6 + " - " + val6);
         updval6 = updval6 - val6;
 
         System.out.println("AUTOGRADER CALCULATING . . . " + (int)updval6 + "/5");
+        System.out.println("");
+        System.out.println("");
+        
+        
     }
 }
